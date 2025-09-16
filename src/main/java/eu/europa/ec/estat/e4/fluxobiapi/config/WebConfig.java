6966b1c5df392b7ee7c9d91a6f8f8bcc927bcd31
kerber <kerber@localhost>
2025-09-16 19:55:13 +0200

    frist commit and deploy

--- src/main/java/eu/europa/ec/estat/e4/fluxobiapi/config/WebConfig.java
+++ src/main/java/eu/europa/ec/estat/e4/fluxobiapi/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:4200",
-                        "https://wise-hitachi-soc-outer.trycloudflare.com"
+                        "https://washing-news-bedford-reviewer.trycloudflare.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
